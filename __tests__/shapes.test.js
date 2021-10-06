0c3fcc6d2ff5645c84bac4805faf52b813b607ed
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-06 14:14:54 +0100

    New test

---
+++ __tests__/shapes.test.js
@@ -0,0 +1,9 @@
+const { Shape, Cube, Sphere } = require("../functions/shapes.js");
+
+describe("Shapes and the use of inheritance", () => {
+    describe("Shape Class", () => {
+        test("Shape class has a constructor, but throws an error when it is used", () => {
+            expect(new Shape()).toThrow("Cannot instantiate abstract class!")
+        })
+    })
+})
