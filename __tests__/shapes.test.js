5eeef39488a421517ea9d898b7100e43a1a67f84
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-07 11:47:12 +0100

    New tests

--- __tests__/shapes.test.js
+++ __tests__/shapes.test.js
@@ -1,9 +1,19 @@
-const { Shape, Cube, Sphere } = require("../functions/shapes.js");
+const { Shape, Cube, Sphere, Dodecahedron } = require("../functions/shapes.js");
 
 describe("Shapes and the use of inheritance", () => {
     describe("Shape Class", () => {
         test("Shape class has a constructor, but throws an error when it is used", () => {
             expect(() => new Shape()).toThrow("Cannot instantiate abstract class!")
+        })
+
+        test("Shape class has a volume method, but throws an error if it is not overwritten by subclasses", () => {
+            const shape = new Dodecahedron(10, 10);
+            expect(() => shape.getVolume()).toThrow("Abstract method must be overwritten by subclass!")
+        })
+
+        test("Shape class has a surfaceArea method, but throws an error if it is not overwritten by subclass", () => {
+            const shape = new Dodecahedron(10, 10);
+            expect(() => shape.getSurfaceArea()).toThrow("Abstract method must be overwritten by subclass!");
         })
     })
 })
