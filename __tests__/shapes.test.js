be891cc26c620ba00f95c9959016b7216eb0f1d3
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-07 14:32:51 +0100

    Correcting tests, now failing with 0 code, and passing with solution

--- __tests__/shapes.test.js
+++ __tests__/shapes.test.js
@@ -3,22 +3,22 @@ const { Shape, Cube, Sphere, Dodecahedron } = require("../functions/shapes.js");
 describe("Shapes and the use of inheritance", () => {
     describe("Shape Class", () => {
         test("Shape class has a constructor, but throws an error when it is used", () => {
-            expect(() => new Shape()).toThrow()
+            expect(() => new Shape()).toThrow("Cannot instantiate abstract class!")
         })
 
         test("Shape class has a getVolume method, but throws an error if it is not overwritten by subclasses", () => {
             const shape = new Dodecahedron(10, 10);
-            expect(() => shape.getVolume()).toThrow();
+            expect(() => shape.getVolume()).toThrow("Abstract method must be overwritten by subclass!");
         })
 
         test("Shape class has a getSurfaceArea method, but throws an error if it is not overwritten by subclass", () => {
             const shape = new Dodecahedron(10, 10);
-            expect(() => shape.getSurfaceArea()).toThrow();
+            expect(() => shape.getSurfaceArea()).toThrow("Abstract method must be overwritten by subclass!");
         })
 
         test("Shape class has a getDensity method, but throws an error if it is not overwritten by subclass!", () => {
             const shape = new Dodecahedron(10, 10);
-            expect(() => shape.getDensity()).toThrow();
+            expect(() => shape.getDensity()).toThrow("Abstract method must be overwritten by subclass!");
         })
     })
 
@@ -34,7 +34,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Cube class overwrites the getVolume() method and returns the correct volume of the object, to 3 decimal places", () => {
             const cube = new Cube(10, 100);
             const cube2 = new Cube(17.2, 50);
-            expect(() => cube.getVolume()).not.toThrow();
+            expect(() => cube.getVolume()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(cube.getVolume()).toBe(1000);
             expect(+cube2.getVolume()).toBe(5088.448);
         })
@@ -42,7 +42,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Cube object overwrites the getSurfaceArea() method and returns the correct surface area of the object, to 3 decimal places", () => {
             const cube = new Cube(10, 100);
             const cube2 = new Cube(25.21, 2000);
-            expect(() => cube.getSurfaceArea()).not.toThrow();
+            expect(() => cube.getSurfaceArea()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(cube.getSurfaceArea()).toBe(600);
             expect(cube2.getSurfaceArea()).toBe(3813.265);
         })
@@ -50,7 +50,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Cube class overwrites the getDensity() method and returns the correct density of the object, to 3 decimal places", () => {
             const cube = new Cube(10, 100);
             const cube2 = new Cube(14, 10000);
-            expect(() => cube.getDensity()).not.toThrow();
+            expect(() => cube.getDensity()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(cube.getDensity()).toBe(0.1);
             expect(cube2.getDensity()).toBe(3.644);
         })
@@ -68,7 +68,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Cube class overwrites the getVolume() method and returns the correct volume of the object, to 3 decimal places", () => {
             const sphere = new Sphere(5, 10);
             const sphere2 = new Sphere(16.3, 20);
-            expect(() => sphere.getVolume()).not.toThrow();
+            expect(() => sphere.getVolume()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(sphere.getVolume()).toBe(523.599);
             expect(sphere2.getVolume()).toBe(18140.591);
         })
@@ -76,7 +76,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Sphere object overwrites the getSurfaceArea() method and returns the correct surface area of the object, to 3 decimal places", () => {
             const sphere = new Sphere(5, 10);
             const sphere2 = new Sphere(13, 301);
-            expect(() => sphere.getSurfaceArea()).not.toThrow();
+            expect(() => sphere.getSurfaceArea()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(sphere.getSurfaceArea()).toBe(314.159);
             expect(sphere2.getSurfaceArea()).toBe(2123.717);
         })
@@ -84,7 +84,7 @@ describe("Shapes and the use of inheritance", () => {
         test("Sphere class overwrites the getDensity() method and returns the correct density of the object, to 3 decimal places", () => {
             const sphere = new Sphere(5, 10);
             const sphere2 = new Sphere(16.3, 19001);
-            expect(() => sphere.getDensity()).not.toThrow();
+            expect(() => sphere.getDensity()).not.toThrow("Abstract method must be overwritten by subclass!");
             expect(sphere.getDensity()).toBe(0.019);
             expect(sphere2.getDensity()).toBe(1.047);
         })
