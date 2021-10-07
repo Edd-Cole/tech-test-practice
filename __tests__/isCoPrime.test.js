050b06aa6a405b9b98fe18abb9e89be7b574efcb
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-07 16:17:46 +0100

    tests for isCoPrime!

---
+++ __tests__/isCoPrime.test.js
@@ -0,0 +1,20 @@
+const isCoPrime = require("../functions/isCoPrime.js");
+
+describe("Are these 2 Numbers Co-Prime?", () => {
+    test("Returns false if the 2 numbers are the same", () => {
+        expect(isCoPrime(5, 5)).toBe(false);
+        expect((isCoPrime(2222, 2222))).toBe(false);
+    })
+
+    test("Returns true if the two numbers are co-prime", () => {
+        expect(isCoPrime(57, 58)).toBe(true);
+        expect(isCoPrime(13, 17)).toBe(true);
+        expect(isCoPrime(15, 1111)).toBe(true);
+    })
+
+    test("Returns false if the 2 numbers are not co-prime", () => {
+        expect(isCoPrime(10000000, 100)).toBe(false);
+        expect(isCoPrime(56, 300279)).toBe(false);
+        expect(isCoPrime(6923544303, 377)).toBe(false);
+    })
+})
