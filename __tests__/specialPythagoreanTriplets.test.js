b4fea6ed8cf49e4a1a56893f65fac88d8adf5a06
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-08 13:46:51 +0100

    New tests for pythag

---
+++ __tests__/specialPythagoreanTriplets.test.js
@@ -0,0 +1,14 @@
+const pythag = require("../functions/specialPythagoreanTriplets.js");
+
+describe("Special Pythagorean Triplets", () => {
+    test("returns 0 if there are no integers a, b and c that sum to n", () => {
+        expect(pythag(6)).toBe(0);
+        expect(pythag(100)).toBe(0);
+    })
+
+    test("returns a product of abc if there are integers a, b and c that sum to n", () => {
+        expect(pythag(24)).toBe(480)
+        expect(pythag(30)).toBe(780)
+        expect(pythag(1000)).toBe(31875000)
+    })
+})
