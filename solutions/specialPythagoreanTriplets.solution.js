b045d35d3a4e9827c17a902a75afc1023375971a
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-08 13:50:24 +0100

    refactoring with notes

--- solutions/specialPythagoreanTriplets.solution.js
+++ solutions/specialPythagoreanTriplets.solution.js
@@ -1,14 +1,21 @@
 const pythag = (n) => {
+    //Loop through to find the first number, a or i
     for (let i = 1; i < n; i++) {
-        for (let j = 1; j < n; j++) {
+        //Loop through to find the second number, b or j
+        for (let j = 1; j < i; j++) {
+            //Calculate c or k, by using c = sqrt(a^2 + b^2);
             let k = Math.sqrt(i * i + j * j);
+            //If k is an integer, we have a pythag triplet!
             if (k % 1 === 0) {
+                //Lastly, check that the pythag triplet we've found satisfies the condition a + b + c = n
                 if (i + k + j === n) {
+                    //Return the product of a * b * c
                     return i * j * k
                 }
             }
         }
     }
+    //If no solution is found, return 0
     return 0;
 }
 
