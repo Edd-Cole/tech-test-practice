4661528d1ec41e7bf9255b0583b4304af903d62d
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-11 13:22:36 +0100

    New tests for tech test

---
+++ __tests__/toggleDoors.test.js
@@ -0,0 +1,13 @@
+const toggleDoors = require("../functions/toggleDoors.js");
+
+describe("Which Doors are Open?", () => {
+    test("When given a number of doors, function returns an array", () => {
+        expect(Array.isArray(toggleDoors(10))).toBe(true);
+        expect(Array.isArray(toggleDoors(100))).toBe(true);
+    })
+
+    test("Returns an array with the index of each door that is open when passed a number of doors", () => {
+        expect(toggleDoors(100)).toEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100])
+        expect(toggleDoors(1000)).toEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900, 961])
+    })
+})
