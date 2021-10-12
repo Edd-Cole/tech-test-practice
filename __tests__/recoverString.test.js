37a9da16ae2dc0de9513d214dc2b7cf56f1561f6
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-12 14:31:20 +0100

    tests for recover secret string

---
+++ __tests__/recoverString.test.js
@@ -0,0 +1,56 @@
+const recoverString = require("../functions/recoverString.js");
+
+describe("Recover the Secret String", () => {
+    test("returns a word of 3 letters", () => {
+        const triples = [
+            ["s", "k", "y"]
+        ];
+        expect(recoverString(triples)).toBe("sky");
+    })
+
+    test("returns a word of 5 letters", () => {
+        const triples = [
+            ["a", "k", "l"],
+            ["k", "l", "e"],
+            ["a", "n", "e"],
+            ["n", "k", "l"]
+        ];
+        expect(recoverString(triples)).toBe("ankle");
+    })
+
+    test("returns a word of 7 letters", () => {
+        const triples = [
+            ["i", "b", "e"],
+            ["a", "l", "b"],
+            ["c", "a", "r"],
+            ["l", "i", "r"],
+            ["b", "r", "e"]
+        ]
+        expect(recoverString(triples)).toBe("calibre");
+    })
+
+    test("returns a word of 15 letters long", () => {
+        triples = [
+            ["g", "l", "p"],
+            ["d", "t", "g"],
+            ["a", "t", "o"],
+            ["p", "h", "s"],
+            ["o", "g", "l"],
+            ["y", "p", "i"],
+            ["d", "c", "s"],
+            ["y", "i", "c"],
+            ["m", "a", "g"],
+            ["e", "r", "t"],
+            ["r", "m", "y"],
+            ["g", "i", "s"],
+            ["d", "e", "c"],
+            ["l", "y", "s"],
+            ["i", "c", "s"],
+            ["t", "o", "i"],
+            ["a", "g", "y"],
+            ["i", "c", "s"],
+            ["h", "i", "c"]
+        ]
+        expect(recoverString(triples)).toBe("dermatoglyphics");
+    })
+})
