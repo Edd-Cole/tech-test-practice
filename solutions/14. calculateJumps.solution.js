2f451efe306359e023046d38a6173a78c488b946
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-30 16:29:18 +0100

    New notes for frog jump

--- solutions/14. calculateJumps.solution.js
+++ solutions/14. calculateJumps.solution.js
@@ -1,4 +1,5 @@
 function calculateJumps(a, b, d) {
+    // Subtract the two distances from each other, then divide by the length of the frogs jump, then raise it to the nearest integer for total jumps
     return Math.ceil((b - a) / d);
 }
 
