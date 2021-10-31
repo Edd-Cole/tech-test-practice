f262f5d069666c3983eafe6473b8d7fcfd5b27df
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-31 11:55:54 +0000

    New tests for tech test

--- __tests__/9. numberCalculator.test.js
+++ __tests__/9. numberCalculator.test.js
@@ -1,7 +1,42 @@
 const NumberCalculator = require("../functions/2. Classes/9. numberCalculator.js");
 
-decribe("Number Calculator", () => {
+describe("Number Calculator", () => {
     test("Constructor creates the desired object", () => {
-        const nums = new NumberCalculator(1, 2, 3);
+        let numbers = new NumberCalculator(1, 2, 3);
+        expect(numbers.nums).toEqual([1, 2, 3])
+        numbers = new NumberCalculator(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+        expect(numbers.nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    })
+
+    test("Generate() adds a new number into the array and returns it", () => {
+        let numbers = new NumberCalculator(1, 2, 3);
+        expect(numbers.generate().nums).toEqual([1, 2, 3, 2]);
+        numbers = new NumberCalculator(2, 4, 6);
+        expect(numbers.generate().nums).toEqual([2, 4, 6, 2]);
+        numbers = new NumberCalculator(2, 2, 2);
+        expect(numbers.generate().nums).toEqual([2, 2, 2, 3]);
+        expect(numbers.generate().nums).toEqual([2, 2, 2, 3, 3]);
+    })
+
+    test("GetMidpoint() returns the midpoint of the values", () => {
+        let numbers = new NumberCalculator(1, 2, 3);
+        expect(numbers.getMidpoint()).toBe(2);
+        numbers.generate().generate();
+        expect(numbers.getMidpoint()).toBe(2)
+        numbers = new NumberCalculator(9, 4, 1);
+        numbers.generate().generate().generate().generate();
+        expect(numbers.getMidpoint()).toBe(4)
+    })
+
+    test("GetUniqueFactors() returns all the unique factors for given object", () => {
+        const numbers = new NumberCalculator(1, 2, 3);
+        expect(numbers.getUniqueFactors()).toEqual([2, 3]);
+        numbers.generate();
+        expect(numbers.getUniqueFactors()).toEqual([3]);
+        const nums = new NumberCalculator(3, 2, 4);
+        expect(nums.getUniqueFactors()).toEqual([3, 4]);
+        nums.generate();
+        nums.generate();
+        expect(nums.getUniqueFactors()).toEqual([]);
     })
 })
