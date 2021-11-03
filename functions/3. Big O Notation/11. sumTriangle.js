e87d68d00ffa8671e08c389dee25f51e5d313e25
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-11-03 13:44:55 +0000

    Updating triangle files

--- functions/3. Big O Notation/11. sumTriangle.js
+++ functions/3. Big O Notation/11. sumTriangle.js
@@ -1,11 +1,3 @@
-const sumTriangle = (n) => {
-    return n * (n + 1) / 2;
-    let sum = 0;
-
-    for (let i = 0; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
-}
+const sumTriangle = () => {}
 
 module.exports = sumTriangle
--- solutions/3. Big O Notation/11. sumTriangle.solution.js
+++ solutions/3. Big O Notation/11. sumTriangle.solution.js
@@ -1,3 +1,6 @@
-const sumTriangle = () => {}
+const sumTriangle = (n) => {
+    // Return formula for "triangle numbers", O(1) efficency, where as any loop will be O(n) efficiency
+    return n * (n + 1) / 2;
+}
 
 module.exports = sumTriangle
