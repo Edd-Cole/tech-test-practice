1288825e70b131b7180a1af41e752bf806855ec9
Edd Cole <edd_kleszcz@hotmail.co.uk>
2021-11-21 20:18:49 +0000

    Resetting function back to empty state

--- functions/1. Intermediate/8. passingNumber.js
+++ functions/1. Intermediate/8. passingNumber.js
@@ -1,14 +1,3 @@
-function passingNumber(number, value) {
-    const regex = new RegExp(`(?=${value})`, "g");
-    let occurencesOfValue = 0;
-
-    for (let i = 0; i <= number; i++) {
-        const occurences = i.toString().match(regex);
-        const countOccurences = occurences ? occurences.length : 0;
-        occurencesOfValue += countOccurences;
-    }
-
-    return occurencesOfValue;
-}
+function passingNumber() {}
 
 module.exports = passingNumber;
