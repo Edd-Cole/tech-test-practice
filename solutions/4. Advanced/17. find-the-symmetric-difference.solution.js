ce8ec6db39363c2031f1473845157259dfccfa77
Edd Cole <edd_kleszcz@hotmail.co.uk>
2021-11-22 23:26:29 +0000

    Refactoring sym solution

--- solutions/4. Advanced/17. find-the-symmetric-difference.solution.js
+++ solutions/4. Advanced/17. find-the-symmetric-difference.solution.js
@@ -1,8 +1,6 @@
 function sym(...args) {
     // Define a 2 arrays and an object
     const sets = [];
-    const values = [];
-    const obj = {};
 
     // Loop through all the arguments and create a Set (a Set is an object that only contains one unique value for each value passed in) 
     // from each argument passed in
@@ -10,24 +8,24 @@ function sym(...args) {
         sets[sets.length] = new Set(args[i]);
     }
 
-    // Loop through all the sets in the sets array
+    // Pop out the first set from the sets array
+    const valueSet = sets.shift();
+
+    // Loop through the rest of the sets in sets array
     for (const set of sets) {
-        // Loop through all the numbers in the set
+        // Loop through each number in each set
         for (const num of set) {
-            // If number exists as a key in obj, set value to false, otherwise set key-value pair to {... , num: true}
-            obj[num] ? obj[num] = false : obj[num] = true;
+            // If our first set contains num, delete it from the first set, otherwise add it into our first set
+            if (valueSet.has(num)) {
+                valueSet.delete(num);
+            } else {
+                valueSet.add(num);
+            }
         }
     }
 
-    // Loop through all the numbers in obj
-    for (let num in obj) {
-        // If the number in object is true, add it into values, casting it back into a number;
-        if (obj[num])
-            values[values.length] = +num;
-    }
-
-    // Return the values array
-    return values;
+    // Return our sets as a true array via destructuring and sort the result in ascending order
+    return [...valueSet].sort((a, b) => a - b);
 }
 
 module.exports = sym;
